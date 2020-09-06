4e9011a729455bb29a975d009112ed51779fe15d
guihunkun <615815574@qq.com>
2020-09-06 20:11:17 +0800

    ListNode

--- LinkedList/ListNode.cpp
+++ LinkedList/ListNode.cpp
@@ -333,9 +333,13 @@ int main()
     int arr[] = {1,2,3,4,5,6};
     int n = sizeof(arr)/sizeof(int);
 
+    cout<<"inital list"<<endl;
     ListNode* head = createLinkedList(arr, n);
     printLinkedList(head);
 
+    ListNode* res = reverseList(head);
+    cout<<"after reverse the list"<<endl;
+    printLinkedList(res);
 
     return 0;
 }
